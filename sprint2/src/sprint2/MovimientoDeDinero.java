e1a0edb1a5415d6c3ab07eafff54a68093fce0c5
Valentina Marin <anav.usuga@gmail.com>
2022-08-28 19:00:24 -0500

    Creacion de la clase main y correccion de errores

---
+++ sprint2/src/sprint2/MovimientoDeDinero.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sprint2;
+
+/**
+ *
+ * @author Monsalve
+ */
+public class MovimientoDeDinero extends Empresa {
+    private double montoMovimiento;
+    private String tipoDeMoviento;
+    private String usuario;
+
+    public MovimientoDeDinero(double montoMovimiento, String tipoDeMoviento, String usuario, String nombreEmpresa, String direccion, int telefono, int nit) {
+        super(nombreEmpresa, direccion, telefono, nit);
+        this.montoMovimiento = montoMovimiento;
+
+        this.tipoDeMoviento = tipoDeMoviento;
+        this.usuario = usuario;
+    }
+
+    public double getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(double montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public String getTipoDeMoviento() {
+        return tipoDeMoviento;
+    }
+
+    public void setTipoDeMoviento(String tipoDeMoviento) {
+        this.tipoDeMoviento = tipoDeMoviento;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+
+    
+    
+    
+}
